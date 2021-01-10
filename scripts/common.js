6c701df460db676035660fd2cc1d7d4e68671d04
Arun Subash Manickam <arunsubashm@gmail.com>
2021-01-10 07:49:52 +0530

    Implement "All Post", Toggle to "signup" page from "signin"

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,17 @@
+/* Modal Form - Diplay and Close */
 function displayForm(id, toggle) {
     if (toggle == 0) {
         document.getElementById(id).style.display='block';
     } else {
         document.getElementById(id).style.display='none';
     }
+}
+/* Open a page in the same window */
+function openPage(page) {
+    window.open(page,'_self');
+}
+/* */
+function swapForm(closeid, displayid) {
+    document.getElementById(closeid).style.display='none';
+    document.getElementById(displayid).style.display='block';
 }
