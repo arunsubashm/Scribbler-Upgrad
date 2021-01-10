38e5e2f42b2f1487da14af7e218a635199718972
Arun Subash Manickam <arunsubashm@gmail.com>
2021-01-10 07:01:29 +0530

    Implementing the signup and signin buttons

---
+++ scripts/common.js
@@ -0,0 +1,7 @@
+function displayForm(id, toggle) {
+    if (toggle == 0) {
+        document.getElementById(id).style.display='block';
+    } else {
+        document.getElementById(id).style.display='none';
+    }
+}
